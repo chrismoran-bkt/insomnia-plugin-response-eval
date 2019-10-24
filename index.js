b88a208906fbb576b3ac8027c18a52fb5c8df3fd
Chris Moran <chris.moran@bakkt.com>
2019-10-24 15:04:38 -0400

    Enhanced Headers: can now support multiple same-named headers ie: 'Set-Cookie'.  - They can be referenced via array index or the output is simply the array itself
    Added prefiltering: Prefilter can now be applied (plain search string or a regular expression (/../). This prefilter is applied to the output. If the regular expression has capture groups, the output becomes the capture groups.

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 const iconv = require('iconv-lite');
+/*
 
+type TemplateTag = {
+  name: string,
+  displayName: DisplayName,
+  disablePreview?: () => boolean,
+  description?: string,
+  deprecated?: boolean,
+  validate?: (value: any) => ?string,
+  priority?: number,
+  args: Array<{
+    displayName: string,
+    description?: string,
+    defaultValue: string | number | boolean,
+    type: 'string' | 'number' | 'enum' | 'model',
+
+    // Only type === 'string'
+    placeholder?: string,
+
+    // Only type === 'model'
+    modelType: string,
+
+    // Only type === 'enum'
+    options: Array<{
+      displayName: string,
+      value: string,
+      description?: string,
+      placeholder?: string,
+    }>,
+  }>,
+};
+
+ */
 module.exports.templateTags = [
   {
     name: 'ResponseEval',
@@ -30,7 +62,14 @@ module.exports.templateTags = [
       {
         type: 'string',
         hide: args => args[0].value !== 'header',
+        defaultValue: '',
         displayName: 'Header Name'
+      },
+      {
+        type: 'string',
+        defaultValue: '',
+        displayName: 'Prefilter',
+        description: 'Pre filter the output. If the output is an array, filter the items by this value (string: indexOf, regex: match(with tokens being returned as the output if they exist)'
       },
       {
         type: 'string',
@@ -40,8 +79,11 @@ module.exports.templateTags = [
       }
     ],
 
-    async run(context, field, id, filter, js) {
-      filter = filter || '';
+    async run(context, field, id, filter, prefilter, js) {
+      filter = field === 'raw' ? '' : filter || '';
+      prefilter = prefilter || '';
+
+      const sanitizedSubFilter = sanitizeSubFilter(prefilter);
 
       if (!['header', 'raw'].includes(field)) {
         throw new Error(`Invalid response field ${field}`);
@@ -72,9 +114,12 @@ module.exports.templateTags = [
         throw new Error(`No ${field} filter specified`);
       }
 
-      let output = ''
+      let output = '', filteredOutput = null;
       if (field === 'header') {
-        output = matchHeader(response.headers, sanitizedFilter);
+        output = matchHeader(response.headers, sanitizedFilter, sanitizedSubFilter);
+        if(output instanceof Array && output.length === 1) {
+          output = output[0];
+        }
       } else if (field === 'raw') {
         const bodyBuffer = context.util.models.response.getBodyBuffer(response, '');
         const match = response.contentType.match(/charset=([\w-]+)/);
@@ -87,11 +132,25 @@ module.exports.templateTags = [
           console.warn('[response] Failed to decode body', err);
           output = bodyBuffer.toString();
         }
+
+        if(output instanceof Array) {
+          filteredOutput = output.filter(b => sanitizedSubFilter.filter(b));
+          filteredOutput = filteredOutput.map(b => sanitizedSubFilter.map(b));
+          if(filteredOutput.length === 1) {
+            filteredOutput = filteredOutput[0];
+          }
+          output = {output, _};
+        } else {
+          if(sanitizedSubFilter.filter(output)) {
+            filteredOutput = sanitizedSubFilter.map(output);
+          }
+        }
       } else {
         throw new Error(`Unknown field ${field}`);
       }
+      console.log('OUTPUT:', {filteredOutput, output});
 
-      let r = output
+      let r = filteredOutput ? filteredOutput : output;
       if (js) {
         try {
           r = eval(js);
@@ -105,17 +164,61 @@ module.exports.templateTags = [
   }
 ];
 
-function matchHeader(headers, name) {
+function sanitizeSubFilter(subFilter) {
+  const sanitizedSubFilter = subFilter.trim();
+  if(sanitizedSubFilter.length === 0) {
+    return {
+      filter: () => true,
+      map: (input) => input
+    };
+  }
+  const subReg = new RegExp('\/(.*)\/([a-zA-Z]*)');
+  if(subReg.test(sanitizedSubFilter)) {
+    console.log('Regex: ', subReg.source);
+    const tags = subReg.exec(sanitizedSubFilter);
+    console.log('FOUND REGEX:', sanitizedSubFilter, tags);
+    const reg = new RegExp(tags[1], tags.length > 2 ? tags[2] : 'g');
+    return {
+      filter: (input) => {
+        return reg.test(input);
+      },
+      map: (input) => {
+        console.log('Checking: ' + input);
+        let res = reg.exec(input);
+        console.log('Returning:', res);
+        return res;
+      }
+    };
+  }
+  return {
+    filter: (input) => {
+      return input.indexOf(sanitizedSubFilter) > -1;
+    },
+    map: (input) => {
+      return input;
+    }
+  }
+}
+
+function matchHeader(headers, name, subFilter) {
   if (!headers.length) {
     throw new Error(`No headers available`);
   }
 
-  const header = headers.find(h => h.name.toLowerCase() === name.toLowerCase());
+  const header = headers.filter(h => h.name.toLowerCase() === name.toLowerCase());
 
-  if (!header) {
+  console.log('HEADER:', header);
+  if (!header || header.length === 0) {
     const names = headers.map(c => `"${c.name}"`).join(',\n\t');
     throw new Error(`No header with name "${name}".\nChoices are [\n\t${names}\n]`);
   }
+  return matchSubHeader(header, subFilter);
+}
 
-  return header.value;
+function matchSubHeader(header, f) {
+  let results = header.filter(h => f.filter(h.value));
+  console.log('Filtered: ', results);
+  let _mapped = results.map(result => f.map(result.value));
+  console.log('Filtered and Mapped: ', _mapped);
+  return _mapped;
 }
